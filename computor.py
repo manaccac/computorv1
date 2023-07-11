1bdbf214f5df8baa1488bf949b876d571a97b63b
Maxime NACCACHE <mnaccache@LT318.local>
2023-07-11 16:17:24 +0200

    -help implemente

--- computor.py
+++ computor.py
@@ -4,17 +4,30 @@ import function as function
 
 if len(sys.argv) != 2:
     print("Please provide an equation as a command line argument")
+    print("Use -help for more information")
     sys.exit(1)
 
 equation = sys.argv[1]
-tmp = equation
 
+if equation == "-help":
+    print("Usage: python3 computor.py equation\n")
+    print("Enter the equation in the following format:")
+    print("- Use '*' for multiplication, e.g., 5*X^2")
+    print("- Use '/' for division, e.g., 2/3*X")
+    print("- Use '^' for exponentiation, e.g., X^3")
+    print("- Separate terms with '+' or '-', e.g., 2*X - 5*X^2 + 3")
+    print("- Place the equation on both sides of the equal sign, e.g., 2*X = 0")
+    sys.exit(0)
+
+tmp = equation
 
 equation = function.syntax(equation)
 
-if (equation == -1 ):
+if equation == -1:
     print("Syntax error")
-    sys.exit()
+    print("Use -help for more information")
+
+    sys.exit(1)
 
 print(tmp)
 equation = equation.split("=")
@@ -24,21 +37,8 @@ left_equation = re.split(r"(\+|-)", left_equation)
 right_equation = re.split(r"(\+|-)", right_equation)
 
 left_equation = function.cut_equation(left_equation)
-
 right_equation = function.cut_equation(right_equation)
-
 equation = function.reduced_form(left_equation, right_equation)
 equation_degree = function.get_degree(equation)
 
-
 function.solve_equation(equation)
-
-
-#5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0
-#5*x^0+4*x+-9.3*x^2=1*x^0		| -1*x^0
-#-9.3*x^2+4*x+4*x^0=0	
-
-#https://www.mathepower.com/fr/equationsdeseconddegree.php
-
-#https://fr.wikipedia.org/wiki/%C3%89quation_du_second_degr%C3%A9
-#https://www.jeuxmaths.fr/cours/equations-premier-degre.php#:~:text=On%20appelle%20%C3%A9quation%20du%20premier,inconnue%20admet%20une%20unique%20solution.
